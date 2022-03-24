189b661c1c11c86f332a8a93a980e809f6abed51
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-24 06:46:27 +0600

    send props from component to component

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Test from '../Test/Test';
 
 const Menubar = ({ country }) => {
     return (
         <div>
             <h1>Hero Alom From {country}</h1>
+            <Test country={'Uganda'}></Test>
         </div>
     );
 };
---
+++ src/components/Test/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = ({ country }) => {
+    return (
+        <div>
+            <h2>Test country is {country}</h2>
+        </div>
+    );
+};
+
+export default Test;
