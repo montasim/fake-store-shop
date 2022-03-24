8c9d04313d1c06decaa76074849e9623dd7d1da2
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-24 06:29:00 +0600

    components folder & Menubar components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 // using bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Menubar from './components/Menubar/Menubar';
 
 function App() {
   return (
     <div className="App">
+      <Menubar></Menubar>
       <h1 className='text-danger'>Hello fake Store Shop</h1>
     </div>
   );
---
+++ src/components/Menubar/Menubar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menubar = () => {
+    return (
+        <div>
+            <h1>Hero Alom</h1>
+        </div>
+    );
+};
+
+export default Menubar;
