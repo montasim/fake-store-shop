aabbfcf31e11e4cc57164acee34a576c06b73c65
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-24 22:31:08 +0600

    hover effect & mobile responsive

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -4,14 +4,16 @@ import './Menubar.css'
 const Menubar = ({ count }) => {
     return (
         <div>
-            <div className="container py-5 ">
-                <div className="row">
-                    <div className="col-md-2">Logo</div>
-                    <div className="col-md-10 menu-container d-flex justify-content-end">
-                        <li className='items me-4 p-2'>Home</li>
-                        <li className='items me-4 p-2'>Contact</li>
-                        <li className='items me-4 p-2'>Cart <sup>{count}</sup></li>
-                        <li className='items me-4 p-2'>Login</li>
+            <div className="container py-5 mx-auto">
+                <div className="d-flex justify-content-around align-items-center">
+                    <div className="container col-md-2 mx-auto">
+                        <h2 className='hover-effect'>LOGO</h2>
+                    </div>
+                    <div className="container col-md-9 menu-container d-flex justify-content-end mx-auto">
+                        <li className='items me-4 p-2 hover-effect'>Home</li>
+                        <li className='items me-4 p-2 hover-effect'>Contact</li>
+                        <li className='items me-4 p-2 hover-effect'>Cart <sup>{count}</sup></li>
+                        <li className='items me-4 p-2 hover-effect'>Login</li>
                     </div>
                 </div>
             </div>
